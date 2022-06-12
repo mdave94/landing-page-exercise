7d649f44422b3672e366d0c8d221f6ef92c4236c
mdave94 <molnardavid2002@gmail.com>
2022-06-12 15:11:19 +0200

    Navbar style update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {FaBars} from 'react-icons/fa'
-
-
 import {Nav,
         NavbarContainer,
         NavLogo, 
         MobileIcon,
         NavMenu,
         NavItem,
-        NavLinks } from './NavbarElements'
+        NavLinks,
+        NavBtn,
+        NavBtnLink } from './NavbarElements'
 
 const Navbar = () => {
   return (
@@ -16,18 +16,28 @@ const Navbar = () => {
         <Nav>
             <NavbarContainer>
                 <NavLogo to='/' >
-                    <h1>Megyez</h1>
+                    Megyez
                 </NavLogo>
                 <MobileIcon>
                     <FaBars />
                 </MobileIcon>
                 <NavMenu>
-                    <NavItem>
-                        <NavLinks to = "about">
-                            About
-                        </NavLinks>
-                    </NavItem>
+                <NavItem>
+                    <NavLinks to='about'>About</NavLinks>    
+                </NavItem> 
+                <NavItem>
+                    <NavLinks to='discover'>Discover</NavLinks>    
+                </NavItem> 
+                <NavItem>
+                    <NavLinks to='services'>Services</NavLinks>    
+                </NavItem>
+                <NavItem>
+                    <NavLinks to='signup'>Sign Up</NavLinks>    
+                </NavItem>
                 </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/signin '>Sign In</NavBtnLink>
+                </NavBtn>
             </NavbarContainer>
         </Nav>
     </>
